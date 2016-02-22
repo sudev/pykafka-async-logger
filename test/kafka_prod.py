f2ae17c5fcfb3edcddb38b4e3bbaa652a38a5187
Sudev Ambadi <sudev.ac@goibibo.com>
2016-02-22 16:37:13 +0530

    Corrected test case

--- test/kafka_prod.py
+++ test/kafka_prod.py
@@ -4,12 +4,17 @@ import sys
 sys.path.append('../src')
 import KafkaHandler as kl
 
+"""
+A very simple test case to test against functionality of this module. 
+Run the test files from test folder with first arugment as Kafka cluster url.
+"""
+
 logger = logging.getLogger('test_application')
 logger.setLevel(logging.DEBUG)
 
 KAFKA_HOST = sys.argv[1]
 # Add the log message handler to the logger
-handler = kl.KafkaLoggingHandler('backup.log', hosts_list=KAFKA_HOST, topic= "testLogger", batch_size=100, key="Hello")
+handler = kl.KafkaLoggingHandler('backup.log', hosts_list=KAFKA_HOST, topic= "testLogger", batch_size=100)
 
 logger.addHandler(handler)
 
