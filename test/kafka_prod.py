11ec103bcdfbe81eaeed20602cb52ff2f7ac44fe
Sudev Ambadi <sudev.ac@goibibo.com>
2016-02-22 16:29:02 +0530

    Fail proof done

--- test/kafka_prod.py
+++ test/kafka_prod.py
@@ -7,9 +7,9 @@ import KafkaHandler as kl
 logger = logging.getLogger('test_application')
 logger.setLevel(logging.DEBUG)
 
-KAFKA_HOST = ''
+KAFKA_HOST = sys.argv[1]
 # Add the log message handler to the logger
-handler = kl.KafkaLoggingHandler(hosts_list=KAFKA_HOST, topic= "testLogger", batch_size=100, key="Hello")
+handler = kl.KafkaLoggingHandler('backup.log', hosts_list=KAFKA_HOST, topic= "testLogger", batch_size=100, key="Hello")
 
 logger.addHandler(handler)
 
@@ -18,4 +18,4 @@ for i in range(200):
     t = time.time()
     logger.info('i = %d' % i)
 # Give some time for Kafka to create connection and produce
-time.sleep(20)
+#time.sleep(20)
