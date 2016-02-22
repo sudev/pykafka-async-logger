b0ea82e796b14502a116047bda28513087ef0a51
Sudev Ambadi <sudev.ac@goibibo.com>
2016-02-22 16:55:57 +0530

    Included ideal producer parameter

--- src/KafkaHandler.py
+++ src/KafkaHandler.py
@@ -5,13 +5,24 @@ import logging
 class KafkaLoggingHandler(logging.Handler):
 
     def __init__(self, hosts_list, topic, batch_size, key=None):
+        """Kafka logger handler attempts to write python logs directly
+        into specified kafka topic instead of writing them into file.
+        """
         logging.Handler.__init__(self)
         self.kafka_client = KafkaClient(hosts_list)
         self.topic = self.kafka_client.topics[topic]
         self.key = bytes(socket.gethostname())
-        self.producer = self.topic.get_producer() 
+        self.producer = topic.get_producer(
+            delivery_reports=True,
+            min_queued_messages=batch_size,
+            max_queued_messages=batch_size * 100,
+            linger_ms=15000,
+            block_on_queue_full=False)
 
     def emit(self, record):
+        """ This method receives logs as parameter record through
+        logging framework, send them to Kafka Cluster
+        """
         # drop kafka logging to avoid infinite recursion
         # Or should I write them to a file ? (Grr.. there are too many of them too, so ignore) 
         if record.name ==  'kafka':
